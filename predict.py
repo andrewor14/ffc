c56dff6f4114e7d6012dcd4a012be6a8d5ade57b
Andrew Or <andrewor14@gmail.com>
2017-03-23 14:27:49 -0400

    K-fold CV with BNB and MNB
    BNB = 75.6%, MNB = 63.7%

--- predict.py
+++ predict.py
@@ -7,44 +7,54 @@ import pandas as pd
 from sklearn.naive_bayes import BernoulliNB, MultinomialNB
 from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
 from sklearn.metrics import roc_curve, auc, f1_score, precision_score, recall_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import KFold, GridSearchCV
 from sklearn.naive_bayes import BernoulliNB, MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 
+base_dir = "data"
+background_file = join(base_dir, "background.out.csv")
+label_file = join(base_dir, "train.out.csv")
+print "Reading in %s..." % background_file
+background_df = pd.read_csv(background_file, low_memory=False)
+print "Reading in %s..." % label_file
+label_df = pd.read_csv(label_file, low_memory=False)
 
 def main():
-  base_dir = "data"
-  background_file = join(base_dir, "background.out.csv")
-  label_file = join(base_dir, "train.out.csv")
-  print "Reading in %s..." % background_file
-  background_df = pd.read_csv(background_file, low_memory=False)
-  print "Reading in %s..." % label_file
-  label_df = pd.read_csv(label_file, low_memory=False)
+  #base_dir = "data"
+  #background_file = join(base_dir, "background.out.csv")
+  #label_file = join(base_dir, "train.out.csv")
+  #print "Reading in %s..." % background_file
+  #background_df = pd.read_csv(background_file, low_memory=False)
+  #print "Reading in %s..." % label_file
+  #label_df = pd.read_csv(label_file, low_memory=False)
+
+  name_to_model = { 
+    'bnb': lambda: BernoulliNB(),
+    'mnb': lambda: MultinomialNB()
+  }
   
-  # TODO: make this k-fold CV
-  num_labels = label_df.shape[0]
-  num_train_examples = int(num_labels * 0.8)
-  train_df = label_df[:num_train_examples]
-  test_df = label_df[num_train_examples:]
-  
-  # TODO: do other labels as well
+  # Do the training!
+  # TODO: test other labels as well
+  n_folds = 5
   label_name = "jobTraining"
-  print "Building train features..."
-  train_features = get_features(train_df, background_df)
-  print "Building train labels..."
-  train_labels = get_label(train_df, label_name)
-  print "Building test features..."
-  test_features = get_features(test_df, background_df)
-  print "Building test labels..."
-  test_labels = get_label(test_df, label_name)
-  
-  # TODO: do other models as well
-  print "Training the model on %s train examples..." % num_train_examples
-  model = BernoulliNB()
-  model.fit(train_features, train_labels)
-  predicted_labels = [float(model.predict_proba([tf])[0][1]) for tf in test_features]
-  expected_labels = test_labels.tolist()
-  print_result(predicted_labels, expected_labels)
+  for model_name in name_to_model:
+    print "Running model '%s'..." % model_name
+    kf = KFold(n_splits = n_folds)
+    result = []
+    for i, (train_indices, test_indices) in enumerate(kf.split(label_df)):
+      print "  - Running on fold %i..." % i
+      train_df = label_df.transpose()[train_indices].transpose()
+      test_df = label_df.transpose()[test_indices].transpose()
+      train_features = get_features(train_df, background_df)
+      test_features = get_features(test_df, background_df)
+      train_labels = get_label(train_df, label_name)
+      test_labels = get_label(test_df, label_name)
+      model = name_to_model[model_name]()
+      model.fit(train_features, train_labels)
+      predicted_labels = [float(model.predict_proba([tf])[0][1]) for tf in test_features]
+      expected_labels = test_labels.tolist()
+      result += [(predicted_labels, expected_labels)]
+    print_result(result)
 
 def get_label(df, label_name):
   '''
@@ -67,24 +77,35 @@ def get_features(label_df, background_df):
     features += [row]
   return np.array(features)
 
-def print_result(predicted_labels, expected_labels):
+def print_result(label_pairs):
   '''
   Convenience method to print the test result of binary classification.
+  :param a list of (predicted_labels, expected_labels) pairs, one for each fold
   '''
-  assert len(predicted_labels) == len(expected_labels)
-  num_correct = 0 
-  rounded_predicted_labels = [round(l) for l in predicted_labels]
-  for i in range(len(predicted_labels)):
-    if rounded_predicted_labels[i] == expected_labels[i]:
-      num_correct += 1
-  percent_correct = float(num_correct) * 100 / len(expected_labels)
-  false_positive_rate, true_positive_rate, thresholds = roc_curve(expected_labels, predicted_labels)
-  roc_auc = auc(false_positive_rate, true_positive_rate)
-  f1 = f1_score(expected_labels, rounded_predicted_labels)
-  precision = precision_score(expected_labels, rounded_predicted_labels)
-  recall = recall_score(expected_labels, rounded_predicted_labels)
+  accuracy, roc_auc, f1, precision, recall = [], [], [], [], []
+  num_labels = len(label_pairs[0][1])
+  for (predicted_labels, expected_labels) in label_pairs:
+    assert len(predicted_labels) == len(expected_labels)
+    num_correct = 0 
+    rounded_predicted_labels = [round(l) for l in predicted_labels]
+    for i in range(len(predicted_labels)):
+      if rounded_predicted_labels[i] == expected_labels[i]:
+        num_correct += 1
+    accuracy += [float(num_correct) * 100 / len(expected_labels)]
+    false_positive_rate, true_positive_rate, thresholds = roc_curve(expected_labels, predicted_labels)
+    roc_auc += [auc(false_positive_rate, true_positive_rate)]
+    f1 += [f1_score(expected_labels, rounded_predicted_labels)]
+    precision += [precision_score(expected_labels, rounded_predicted_labels)]
+    recall += [recall_score(expected_labels, rounded_predicted_labels)]
+  # Take the average of the things
+  accuracy = float(sum(accuracy)) / len(accuracy)
+  roc_auc = float(sum(roc_auc)) / len(roc_auc)
+  f1 = float(sum(f1)) / len(f1)
+  precision = float(sum(precision)) / len(precision)
+  recall = float(sum(recall)) / len(recall)
+  num_correct = int(num_labels * accuracy / 100)
   print "\n================================"
-  print "You guessed %s/%s = %s%% correct." % (num_correct, len(expected_labels), percent_correct)
+  print "You guessed %s/%s = %.3f%% correct." % (num_correct, num_labels, accuracy)
   #print "  - False positive rate: %s" % false_positive_rate.tolist()
   #print "  - True positive rate: %s" % true_positive_rate.tolist()
   #print "  - Thresholds: %s" % thresholds.tolist()
